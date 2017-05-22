d6afe678f3db67b2f23bc6219bbebd21d0c7a33a
Fritz <fpichardom@yahoo.com>
2017-05-22 16:26:10 -0230

    Add working version of id search

--- updateSynTaxa.py
+++ updateSynTaxa.py
@@ -9,6 +9,7 @@ Created on Mon May 22 14:52:09 2017
 import csv
 import os.path
 import sys
+from pymongo import MongoClient
 
 # Function Definition Start
 def check_file(file):
@@ -35,9 +36,24 @@ def clean_name(taxon):
             taxonlist.remove(i)
             return "".join(taxonlist)
     return "".join(taxon.split())
+
+
+
+def id_search(fullname):
+    query = database['fullname_view'].find_one({ "fullname": fullname },{"_id":1})
+    if query is None:
+        sys.exit('%s not in database!'%fullname)
+    else:
+        return query['_id']
+
+
 # Function Definition End
 
 
+client = MongoClient('localhost', 27017)
+database = client['euterpeaedb']
+taxa = database['taxa']
+full_view = database['fullname_view']
 
 input_file = check_file(input("Input file: "))
 

ee4acee873fe5aa4219cbac5d3d9e6b1b6bd5d24
Fritz <fpichardom@yahoo.com>
2017-05-22 18:45:32 -0230

    New version of iplant to json
    Adds new functionality to the script allowing to add to the json file both the synonym and the accepted name, but the accepted version is not in the final text file.
     Changes to be committed:
    	modified:   iplant2json.py

--- iplant2json.py
+++ iplant2json.py
@@ -2,10 +2,48 @@
 
 import csv
 import json
-#import sys
+import sys
+import os.path
 #print(sys.argv[1:])
 #file = input(file: )
-input_file = input("Input file: ")
+
+def clean_syn(value):    
+    if value == 'Accepted':
+        return False
+    elif value =='Synonym':
+        return True
+    else:
+        return value
+    
+def clean_source(value):
+    return value.split(';')
+    
+def clean_fullname(value):
+    p = value.split()        
+    for i in ['subsp.','subsp','var.','var']:
+        if i in p:
+            p = value.split()
+            return {'genus':p[0],'species':p[1],'infraRank':p[2],'infraTaxon':p[3]}
+    return {'genus':p[0],'species':p[1]}
+    
+
+def check_file(file):
+    exists = True
+    newfile = file
+    while exists:
+        if os.path.isfile(newfile):
+            exists = False
+        else:
+            again = input('File %s does not exists\nDo yo want to enter a new file (y/n):'%newfile)
+            if again in ['y','Y','yes','Yes']:
+                newfile = input('Enter a new file name:' )
+            else:                
+                sys.exit('Program closed')
+                
+                
+    return newfile   
+
+input_file = check_file(input("Input file: "))
 output_json = input("Output json file: ")
 output_splist = input("Output species list: ")
 
@@ -22,27 +60,30 @@ with open(input_file,'r') as csvfile, open(output_json + '.json','w') as out_jso
         'Infraspecific_rank':'infraRank',
         'Infraspecific_epithet_matched':'infraTaxon',
 	    'Name_matched_author':'taxonAuthor',
-        'Taxonomic_status':'synomym',
+        'Taxonomic_status':'synonym',
         'Source':'source'
     }
     fields = fieldMap.keys()
     taxa =[]
     out_json.write('[\n')
-    for row in reader:
+    for row in reader:       
+        if row['Taxonomic_status'] == 'Synonym':
+            taxonomy ={}
+            taxonomy['family'] = row['Accepted_name_family']
+            taxonomy.update(clean_fullname(row['Accepted_name']))
+            taxonomy['taxonAuthor'] = row['Accepted_name_author']
+            taxonomy['synonym'] = False
+            taxonomy['source'] = clean_source(row['Source'])
+            out_json.write(json.dumps(taxonomy,ensure_ascii=False)+',\n')
         taxonomy ={}
         for field,value in row.items():
             #print(field,value)
             if field in fields and not value == "":
                 taxonomy[fieldMap[field]] = value
                 if field == 'Taxonomic_status':
-                    if value == 'Accepted':
-                        taxonomy[fieldMap[field]] = False
-                    elif value =='Synonym':
-                        taxonomy[fieldMap[field]] = True
-                    else:
-                        continue
+                    taxonomy[fieldMap[field]] = clean_syn(value)
                 if field == 'Source':
-                    taxonomy[fieldMap[field]] = value.split()
+                    taxonomy[fieldMap[field]] = clean_source(value)
         fullname = [taxonomy['genus'],taxonomy['species'],taxonomy.get('infraTaxon',"")]
         out_splist.write("".join(fullname) + '\n')
         out_json.write(json.dumps(taxonomy,ensure_ascii=False)+',\n')

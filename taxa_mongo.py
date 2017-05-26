4f359adba324f7d3071969b115772713c8d1c51e
Fritz <fpichardom@yahoo.com>
2017-05-26 14:49:35 -0230

    Try insert and update taxa in same module

--- taxa_mongo.py
+++ taxa_mongo.py
@@ -9,6 +9,7 @@ import datetime
 from pymongo import MongoClient
 # function declaration stage
 
+## Database connection
 
 def connect_db(database, collection, client=MongoClient('localhost', 27017)):
     '''
@@ -18,7 +19,9 @@ def connect_db(database, collection, client=MongoClient('localhost', 27017)):
     col = dbase[collection]
     return col
 
-def clean_fullname(value):
+## Accessory functions
+
+def fullname_insert(value):
     '''
     Parse a string with the fullname of a taxon into its separate components
     '''
@@ -30,14 +33,16 @@ def clean_fullname(value):
         'verbatimTaxonRank':parse[2], 'infraspecificEpithet':parse[3]}
     else:
         return {'genus':parse[0]}
-# alternative parsing method
-'''
+
+def fullname_update(taxon):
+    taxonlist = taxon.split()       
     for i in ['subsp.','subsp','var.','var']:
-        if i in p:
-            p = value.split()
-            return {'genus':p[0],'species':p[1],'infraRank':p[2],'infraTaxon':p[3]}
-    return {'genus':p[0],'specificEpithet':p[1]}
-'''
+        if i in taxonlist:            
+            taxonlist.remove(i)
+            return "".join(taxonlist)
+    return "".join(taxonlist)
+
+
 
 def insert_taxa(taxon, collection, cataloger='Pichardo-Marcano, Fritz'):
     '''
@@ -56,9 +61,14 @@ def insert_taxa(taxon, collection, cataloger='Pichardo-Marcano, Fritz'):
         print("%s skipped!"%fullname)
 
 
+def id_search(fullname, collection):
+    query = collection.find_one({ "fullname": fullname },{"_id":1})
+    return query['_id']
 
 
-def run_body(input_file, collection, field_map, output_splist='splist.txt'):
+## Main taxa parse and insert
+
+def run_body(input_file, insert_collection, field_map, output_splist='splist.txt'):
     '''
     Main program for parsing and inserting taxon data into the mongodb database
     '''
@@ -73,8 +83,8 @@ def run_body(input_file, collection, field_map, output_splist='splist.txt'):
                 taxonomy['taxonRank'] = row['Accepted_name_rank']
                 taxonomy['nameAccordingTo'] = row['Source'].split(';')
                 taxonomy['taxonomicStatus'] = "accepted"
-                taxonomy.update(clean_fullname(row['Accepted_name']))
-                insert_taxa(taxonomy, collection)
+                taxonomy.update(fullname_insert(row['Accepted_name']))
+                insert_taxa(taxonomy, insert_collection)
             taxonomy = {}
             for field, value in row.items():
                 if field in general_fields and not value == "":
@@ -83,7 +93,24 @@ def run_body(input_file, collection, field_map, output_splist='splist.txt'):
                         taxonomy[field_map[field]] = value.lower()
                     if field == 'Source':
                         taxonomy[field_map[field]] = value.split(';')
-            insert_taxa(taxonomy, collection)
+            insert_taxa(taxonomy, insert_collection)
             fullname = "".join([taxonomy['genus'], taxonomy.get('specificEpithet', ""),\
             taxonomy.get('infraspecificEpithet', "")])
             splist.write(fullname + '\n')
+
+## Main parse and update synonyms
+
+
+def update_synonym(input_file):
+    with open(input_file,'r') as csvfile:
+    reader = csv.DictReader(csvfile,delimiter='\t')
+    #taxai= []
+    for row in reader:
+        if row['Taxonomic_status'] == 'Synonym':
+            syn = id_search(fullname_update(row['Name_matched']))
+            acc = id_search(fullname_update(row['Accepted_name']))
+            collection.update_one({"_id":syn},{"$set":{"acceptedNameUsage": acc}})
+            #taxonomy =[syn,acc]
+            #taxai.append(taxonomy)
+
+
